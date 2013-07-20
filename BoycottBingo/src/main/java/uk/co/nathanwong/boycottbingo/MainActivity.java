a7ff70e9fa65a1b8f68ed735a75c79529b5a9252
Nathan Wong <nathan@nathanwong.co.uk>
2013-07-20 13:55:28 +0100

    Update icon, flesh out transitions

--- BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/MainActivity.java
+++ BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/MainActivity.java
@@ -1,14 +1,18 @@
 package uk.co.nathanwong.boycottbingo;
 
-import android.graphics.Color;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -16,7 +20,10 @@ import java.util.List;
 
 public class MainActivity extends Activity {
 
-    GridView grid;
+    List<String> list;
+    LinearLayout rows;
+    public int count = 0;
+    Context c;
 
     private static final int BINGO_SIZE = 9;
 
@@ -25,11 +32,34 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        c = this;
+
         String[] strings = getResources().getStringArray(R.array.boycottisms);
-        List<String> list = Arrays.asList(strings);
+        list = Arrays.asList(strings);
         Collections.shuffle(list);
 
-        LinearLayout rows = (LinearLayout) findViewById(R.id.main_rows);
+        rows = (LinearLayout) findViewById(R.id.main_rows);
+        this.createBingo();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    public boolean onRefreshButtonPress(MenuItem item) {
+        rows.removeAllViews();
+        this.createBingo();
+
+        return true;
+    }
+
+    private void createBingo() {
+        Collections.shuffle(list);
 
         LinearLayout ll = new LinearLayout(this);
         ll.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1.0f));
@@ -49,9 +79,56 @@ public class MainActivity extends Activity {
 
             TextView text = new TextView(this);
             text.setText(list.get(i));
+            text.setBackgroundDrawable(getResources().getDrawable(R.drawable.selecttransition));
             text.setPadding(10, 10, 10, 10);
             text.setLayoutParams(new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f));
             text.setGravity(Gravity.CENTER);
+            text.setSelected(false);
+            text.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    TransitionDrawable transition = (TransitionDrawable) view.getBackground();
+
+                    if (!view.isSelected()) {
+                        // Unselected
+                        transition.startTransition(200);
+                        view.setSelected(true);
+                        count++;
+                    } else {
+                        transition.reverseTransition(200);
+                        view.setSelected(false);
+                        count--;
+                    }
+
+                    if (count == 9) {
+                        // Success!
+                        Toast.makeText(c, "Congratulations!", Toast.LENGTH_LONG).show();
+                        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+                        builder.setTitle("Bingo!");
+                        builder.setIcon(R.drawable.ic_fav);
+                        builder.setMessage("Congratulations! Do you want to play again?")
+                                .setCancelable(false)
+                                .setPositiveButton("Yes!", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        MainActivity.this.onRefreshButtonPress(null);
+                                    }
+                                })
+                                .setNegativeButton("No thanks", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.cancel();
+                                    }
+                                });
+
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+
+                }
+            });
+
+            text.setTag(i);
 
             ll.addView(text);
 
@@ -59,15 +136,6 @@ public class MainActivity extends Activity {
         }
 
         rows.addView(ll);
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
     
 }
