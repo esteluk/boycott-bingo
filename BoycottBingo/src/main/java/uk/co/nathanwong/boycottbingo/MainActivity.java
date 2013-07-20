edf617df69f47fc5d157548d9c10f1eb6b204b69
Nathan Wong <nathan@nathanwong.co.uk>
2013-07-20 14:39:20 +0100

    Reset counter!

--- BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/MainActivity.java
+++ BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends Activity {
     }
 
     private void createBingo() {
+        count = 0;
         Collections.shuffle(list);
 
         LinearLayout ll = new LinearLayout(this);
