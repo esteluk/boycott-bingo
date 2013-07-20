5f44cacdf8a71d2c33b5e84268107b40ec5a9a74
Nathan Wong <nathan@nathanwong.co.uk>
2013-07-20 07:35:53 +0100

    Add adapter

---
+++ BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/BoycottAdapter.java
@@ -0,0 +1,49 @@
+package uk.co.nathanwong.boycottbingo;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by nathan on 20/07/2013.
+ */
+public class BoycottAdapter extends BaseAdapter {
+    private Context mContext;
+    public List<String> mStrings;
+
+    public BoycottAdapter(Context context, List<String> strings) {
+        mContext = context;
+        mStrings = strings;
+    }
+
+    public int getCount() {
+        return 9;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView text = null;
+        if (convertView == null) {
+            // If it's not recycled, we'll initialise it
+            text = new TextView(mContext);
+            text.setText(mStrings.get(position));
+            text.setPadding(10, 10, 10, 10);
+        } else {
+            text = (TextView) convertView;
+        }
+
+        return text;
+    }
+
+}
--- BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/MainActivity.java
+++ BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/MainActivity.java
@@ -3,13 +3,27 @@ package uk.co.nathanwong.boycottbingo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.GridView;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends Activity {
+
+    GridView grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] strings = getResources().getStringArray(R.array.boycottisms);
+        List<String> list = Arrays.asList(strings);
+        Collections.shuffle(list);
+
+        grid = (GridView) findViewById(R.id.main_gridView);
+        grid.setAdapter(new BoycottAdapter(this, list));
     }
 
 
