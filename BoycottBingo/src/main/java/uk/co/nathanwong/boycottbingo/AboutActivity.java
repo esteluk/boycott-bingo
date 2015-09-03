da38f6b7729027be133e680cf12beaf99d16abc2
Nathan Wong <nathan@nathanwong.co.uk>
2015-09-03 01:49:24 +0100

    Style the about screen

--- BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/AboutActivity.java
+++ BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/AboutActivity.java
@@ -1,15 +1,34 @@
 package uk.co.nathanwong.boycottbingo;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by nathan on 20/07/2013.
- */
-public class AboutActivity extends Activity {
+public class AboutActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
