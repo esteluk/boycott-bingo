3c280ea500ac3c11f06e631c0acc399240b9f713
Nathan Wong <nathan@nathanwong.co.uk>
2019-10-31 14:14:29 +0000

    Minor rewrite of data provider function

--- BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/models/BingoStringArrayDataProvider.kt
+++ BoycottBingo/src/main/java/uk/co/nathanwong/boycottbingo/models/BingoStringArrayDataProvider.kt
@@ -2,17 +2,13 @@ package uk.co.nathanwong.boycottbingo.models
 
 import uk.co.nathanwong.boycottbingo.interfaces.BingoDataProvider
 import uk.co.nathanwong.boycottbingo.interfaces.BingoSquare
-import java.util.*
 
 class BingoStringArrayDataProvider(stringData: List<String>, private val numberOfSquares: Int) : BingoDataProvider {
 
-    private var data = stringData.map { BingoSquareStringImpl(it) }
+    private val data = stringData.map { BingoSquareStringImpl(it) }
 
     override fun randomBingoSquares(): List<BingoSquare> {
-        data = data.shuffled()
-        val randomCollection = ArrayList<BingoSquare>()
-        data.subList(0, numberOfSquares).forEach { randomCollection.add(it.copy()) }
-        return randomCollection
+        return data.shuffled().take(numberOfSquares).map { it.copy() }
     }
 
 }
